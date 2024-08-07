74da36b10f93b4e833dda8dfdd583ecbfd63e269
Subramanian-NG <subbu1150@gmail.com>
2024-08-07 00:24:07 -0400

    env removed

--- src/app/university/university.service.ts
+++ src/app/university/university.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversityService {
-  private apiUrl = environment.apiUrl;
+  private apiUrl = 'http://universities.hipolabs.com/search';
 
   constructor(private http: HttpClient) { }
 
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://universities.hipolabs.com/search'
-  };
-  
